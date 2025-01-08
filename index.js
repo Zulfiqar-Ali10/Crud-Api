5046e26c2aadc955c5960bda98135ec4d120056b
Zulfiqar Ali <zulfiqar.dev01@gmail.com>
2025-01-08 08:23:20 +0500

    changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import 'dotenv/config'
 import morgan from "morgan";
 import mongoose from "mongoose";
 import bannerRoutes from "./routers/banner.js";
-const cors = require('cors')
+import cors from "cors"
 
 const app = express();
 const PORT = 5000;
@@ -31,11 +31,11 @@ mongoose.connect(process.env.MONGODBURI)
 .then(()=> console.log("Mongodb Connect"))
 .catch((err)=> console.log("err", err));
 
-// app.get('/', (req, res) => {
-//     console.log("req=>" , req);
-//     res.send("Hello world");
-// })
+app.get('/', (req, res) => {
+    console.log("req=>" , req);
+    res.send("Hello world");
+})
 
-app.use("/", bannerRoutes)
+app.use("/banner", bannerRoutes)
 
 app.listen(PORT, ()=> console.log("PORT Is Running On Server" + PORT));
